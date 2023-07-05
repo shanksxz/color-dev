92c591c09883a22768eefaca71d029110c7be578
Somya Bhatt <somyacu1@gmail.com>
2023-07-05 23:37:50 +0530

    done

--- script.js
+++ script.js
@@ -16,33 +16,39 @@ const x_displayNo = get('x-displayno');
 const y_displayNo = get('y-displayno');
 const br_displayNo = get('br-displayno');
 const sr_displayNo = get('sr-displayno');
+const bg_color = get('bgcolor');
+const bgDisplay = get("bg");
+const codeToCopy = get("generate");
+let count;
 
+box.style.boxShadow = "0px 0px 50px 0px #DDE6ED"
 
 function UpdateShadows() {
     const code = `${offsetxNo.value}px ${offsetyNo.value}px ${blurNo.value}px ${spreadNo.value}px ${color.value}`;
     count = `box-shadow : ${code}`;
     box.style.boxShadow = code;
     box.style.backgroundColor = boxcolor.value;
+    codeToCopy.textContent = "GenerateCode"
 }
 
 const display = [
-    [offsetxNo,offsetxDisplay],
-    [offsetyNo,offsetyDisplay],
-    [blurNo,blurRadiusDisplay],
-    [spreadNo,spreadRadiusDisplay],
+    [offsetxNo, offsetxDisplay],
+    [offsetyNo, offsetyDisplay],
+    [blurNo, blurRadiusDisplay],
+    [spreadNo, spreadRadiusDisplay],
 ]
 
 
 const input = [
-    [x_displayNo,offsetxNo,offsetxDisplay],
-    [y_displayNo,offsetyNo,offsetyDisplay],
-    [br_displayNo,blurNo,blurRadiusDisplay],
-    [sr_displayNo,spreadNo,spreadRadiusDisplay],
+    [x_displayNo, offsetxNo, offsetxDisplay],
+    [y_displayNo, offsetyNo, offsetyDisplay],
+    [br_displayNo, blurNo, blurRadiusDisplay],
+    [sr_displayNo, spreadNo, spreadRadiusDisplay],
 ]
 
 input.forEach(e => {
-    e[0].addEventListener("input",(event)=>{
-        if(event.target.value != ""){
+    e[0].addEventListener("input", (event) => {
+        if (event.target.value != "") {
             e[1].value = event.target.value;
             e[2].textContent = `${event.target.value}px`
         }
@@ -54,21 +60,51 @@ input.forEach(e => {
 })
 
 
+bg_color.addEventListener("input", () => {
+    bgDisplay.style.backgroundColor = bg_color.value;
+})
+
 
 display.forEach(e => {
-    e[0].addEventListener("input", (event)=>{
+    e[0].addEventListener("input", (event) => {
         e[1].textContent = `${event.target.value}px`;
     })
 })
 
-const numbers = [offsetxNo,offsetyNo,blurNo,spreadNo,color,boxcolor];
+const numbers = [offsetxNo, offsetyNo, blurNo, spreadNo, color, boxcolor];
 numbers.forEach(element => {
-    element.addEventListener("input",  UpdateShadows);
+    element.addEventListener("input", UpdateShadows);
 })
 
-function GenerateCode() {
-    console.log(count);
-}
+// function GenerateCode() {
+// console.log(count);
+// codeToCopy.addEventListener("click", copyContent());
+
+// async function copyContent() {
+//     try {
+//         await navigator.clipboard.writeText(count);
+//         // copyMsg.innerText = "copied";
+//         console.log("copied");
+//     }
+//     catch (e) {
+//         // copyMsg.innerText = "Failed";
+//         console.log("failed");
+//     }
+// }
+
+codeToCopy.addEventListener('click', async event => {
+    if (!navigator.clipboard) {
+      // Clipboard API not available
+      return
+    }
+    const text = count;
+    try {
+      await navigator.clipboard.writeText(text)
+      event.target.textContent = 'Copied to clipboard'
+    } catch (err) {
+      console.error('Failed to copy!', err)
+    }
+  })
 
 
 
